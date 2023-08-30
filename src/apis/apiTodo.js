9695c166a21467d496f0f1e7540b5778e5b2aadf
tsaaiiiii <a0909513991@gmail.com>
2023-08-30 15:57:03 +0800

    fix api

--- src/apis/apiTodo.js
+++ src/apis/apiTodo.js
@@ -2,7 +2,7 @@ import { request } from '@/apis/request.js'
 
 // 取得-get
 const api_todoGet = () => {
-  return request.get()
+  return request.get(import.meta.env.VITE_API_URL)
 }
 // 新增-post
 const api_todoPost = (content) => {
@@ -10,11 +10,11 @@ const api_todoPost = (content) => {
 }
 // 刪除-delete
 const api_todoDelete = (id) => {
-  return request.delete(`${id}`)
+  return request.delete(import.meta.env.VITE_API_URL, `${id}`)
 }
 // 切換-patch
 const api_todoPatch = (id) => {
-  return request.patch(`${id}`)
+  return request.patch(import.meta.env.VITE_API_URL, `${id}`)
 }
 
 export { api_todoGet, api_todoPost, api_todoDelete, api_todoPatch }
