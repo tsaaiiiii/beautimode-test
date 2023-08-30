720aa8853f2460d0587c3d7574f62437ea3e0873
tsaaiiiii <a0909513991@gmail.com>
2023-08-30 18:57:29 +0800

    fix api

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify('https://jsonplaceholder.typicode.com/todos')
+  },
   base: '/beautimode-test/',
   plugins: [vue()],
   resolve: {
