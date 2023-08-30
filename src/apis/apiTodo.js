bb0e11b06a1e3e71d5f90094245f6ae98987efdc
tsaaiiiii <a0909513991@gmail.com>
2023-08-30 16:26:01 +0800

    fix api request

--- src/apis/apiTodo.js
+++ src/apis/apiTodo.js
@@ -1,19 +1,39 @@
 import axios from 'axios'
 
 const api_todoGet = () => {
-  return axios.get(import.meta.env.VITE_API_URL)
+  return axios.get(import.meta.env.VITE_API_URL, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
 }
 
 const api_todoPost = (content) => {
-  return axios.post(import.meta.env.VITE_API_URL, { content })
+  return axios.post(
+    import.meta.env.VITE_API_URL,
+    { content },
+    {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }
+  )
 }
 
 const api_todoDelete = (id) => {
-  return axios.delete(`${import.meta.env.VITE_API_URL}/${id}`)
+  return axios.delete(`${import.meta.env.VITE_API_URL}/${id}`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
 }
 
 const api_todoPatch = (id) => {
-  return axios.patch(`${import.meta.env.VITE_API_URL}/${id}`)
+  return axios.patch(`${import.meta.env.VITE_API_URL}/${id}`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
 }
 
 export { api_todoGet, api_todoPost, api_todoDelete, api_todoPatch }
