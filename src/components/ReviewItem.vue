215de9026497c1c0e2c3e5c74a17dc23244def7a
tsaaiiiii <a0909513991@gmail.com>
2023-08-30 19:04:08 +0800

    fix api

--- src/components/ReviewItem.vue
+++ src/components/ReviewItem.vue
@@ -2,7 +2,7 @@
   <div class="py-40 bg-slate-50 flex justify-center h-[100vh]">
     <div>
       <h2 class="font-bold mb-2 text-2xl">題目3</h2>
-      <ul class="mb-10">
+      <ul class="mb-10 leading-10">
         <li>1.命名具有一致性</li>
         <li>2.使用變數管理css</li>
         <li>3.可能避免以 value 作為變數名稱</li>
@@ -12,7 +12,7 @@
         <li>7.命名可能可以再簡短</li>
       </ul>
       <h2 class="font-bold mb-2 text-2xl">題目4</h2>
-      <ul>
+      <ul class="leading-10">
         <li>1.v-text可以改為{{}}</li>
         <li>2.const toggle的函式裡，可能可以改成if/if else使用</li>
         <li>3.命名可能可以再簡短</li>
